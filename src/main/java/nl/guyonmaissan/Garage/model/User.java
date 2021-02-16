ef35284bc1ece823b1c1e54cf7aca19226c3d815
Guyon95 <guyon@maissan.com>
2021-02-16 23:07:53 +0100

    Created user model. Closes #6

---
+++ src/main/java/nl/guyonmaissan/Garage/model/User.java
@@ -0,0 +1,43 @@
+package nl.guyonmaissan.Garage.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String email;
+
+    @Getter
+    @Setter
+    private String username;
+
+    @Getter
+    @Setter
+    private String password;
+
+    @ManyToMany
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+
+}

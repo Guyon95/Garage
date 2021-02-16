71d87dde22c2afbb5c2484a0c17b6bf28f136f94
Guyon95 <guyon@maissan.com>
2021-02-16 23:08:11 +0100

    Created role model. Closes #7

---
+++ src/main/java/nl/guyonmaissan/Garage/model/Role.java
@@ -0,0 +1,28 @@
+package nl.guyonmaissan.Garage.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Role {
+
+    @Id
+    @GeneratedValue(
+            strategy= GenerationType.AUTO,
+            generator="native"
+    )
+    @GenericGenerator(
+            name = "native",
+            strategy = "native"
+    )
+    @Column(columnDefinition = "serial")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ERole name;
+}

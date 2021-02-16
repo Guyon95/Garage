b726197c364bd44435558b5c3577ee41e37ab7a5
Guyon95 <guyon@maissan.com>
2021-02-16 23:07:06 +0100

    Created user model. Closes #11

---
+++ src/main/java/nl/guyonmaissan/Garage/model/Labor.java
@@ -0,0 +1,36 @@
+package nl.guyonmaissan.Garage.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Labor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String description;
+
+    @Getter
+    @Setter
+    private double price;
+
+    @Getter
+    @Setter
+    private LocalDateTime created;
+
+    @Getter
+    @Setter
+    private LocalDateTime modified;
+}

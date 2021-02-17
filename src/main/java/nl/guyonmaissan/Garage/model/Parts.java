7790528f5338f2bf38930a25e0e04d0faac8ed04
Guyon95 <guyon@maissan.com>
2021-02-17 18:17:13 +0100

    Add Entity annotation

--- src/main/java/nl/guyonmaissan/Garage/model/Parts.java
+++ src/main/java/nl/guyonmaissan/Garage/model/Parts.java
@@ -3,11 +3,13 @@ package nl.guyonmaissan.Garage.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
+@Entity
 public class Parts {
 
     @Id

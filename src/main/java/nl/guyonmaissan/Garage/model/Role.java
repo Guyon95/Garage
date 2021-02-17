3123fae14694a3a4d78d194d21cff91003c8be4d
Guyon95 <guyon@maissan.com>
2021-02-17 18:16:09 +0100

    FIX Many-to-Many user_role.

--- src/main/java/nl/guyonmaissan/Garage/model/Role.java
+++ src/main/java/nl/guyonmaissan/Garage/model/Role.java
@@ -1,28 +1,36 @@
 package nl.guyonmaissan.Garage.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.List;
 
+@Entity(name = "Role")
+@Table(name = "role")
 public class Role {
 
     @Id
-    @GeneratedValue(
-            strategy= GenerationType.AUTO,
-            generator="native"
-    )
-    @GenericGenerator(
-            name = "native",
-            strategy = "native"
-    )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(columnDefinition = "serial")
-    private Long id;
+    @Getter
+    @Setter
+    private Long role_id;
 
     @Enumerated(EnumType.STRING)
+    @Getter
+    @Setter
     private ERole name;
+
+    @ManyToMany (mappedBy = "roles")
+    private List<User> users;
 }
--- src/main/java/nl/guyonmaissan/Garage/model/User.java
+++ src/main/java/nl/guyonmaissan/Garage/model/User.java
@@ -3,6 +3,7 @@ package nl.guyonmaissan.Garage.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,16 +11,20 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter
     @Setter
-    private Long id;
+    private Long user_id;
 
     @Getter
     @Setter
@@ -37,7 +42,7 @@ public class User {
     @JoinTable(name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles;
+    private List<Role> roles;
 
 
 }

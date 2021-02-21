1e66d7ee90e6c32cf624fc2c97b00720afef782f
Guyon95 <guyon@maissan.com>
2021-02-21 22:15:47 +0100

    Updated Controller Endpoints

--- src/main/java/nl/guyonmaissan/Garage/controller/CustomerController.java
+++ src/main/java/nl/guyonmaissan/Garage/controller/CustomerController.java
@@ -1,10 +1,12 @@
 package nl.guyonmaissan.Garage.controller;
 
 import nl.guyonmaissan.Garage.model.Customer;
+import nl.guyonmaissan.Garage.payload.response.MessageResponse;
 import nl.guyonmaissan.Garage.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,10 +26,22 @@ public class CustomerController {
     CustomerService customerService;
 
 
+    @GetMapping(value = "")
+    @PreAuthorize("hasRole('ADMINISTRATOR')")
+    public ResponseEntity<Object> getCustomers() {
+        return ResponseEntity.ok().body(customerService.getAllCustomers());
+    }
+
     @GetMapping(value = "/{id}")
     @PreAuthorize("hasRole('ADMINISTRATOR')")
     public ResponseEntity<Object> getCustomer(@PathVariable("id") long id) {
         return ResponseEntity.ok().body(customerService.getCustomerById(id));
+    }
+
+    @GetMapping(value = "/licenseplate/{licensePlate}")
+    @PreAuthorize("hasRole('ADMINISTRATOR')")
+    public ResponseEntity<Object> getCustomer(@PathVariable("licensePlate") String licensePlate) {
+        return ResponseEntity.ok().body(customerService.getCustomerByLicensePlate(licensePlate));
     }
 
     @PostMapping(value = "")
@@ -46,5 +60,16 @@ public class CustomerController {
                 .buildAndExpand(newId).toUri();
 
         return ResponseEntity.created(location).body(location);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<MessageResponse> updateCustomer(@RequestBody Customer customer) {
+        return customerService.updateCustomer(customer);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Object> deleteCourse(@PathVariable("id") long id) {
+        customerService.deleteCustomer(id);
+        return ResponseEntity.noContent().build();
     }
 }
